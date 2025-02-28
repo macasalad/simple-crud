d235bbacc6a65a695a4919fcee4f5eba819166dc
macasalad <hannahleibaniqued12@gmail.com>
2025-03-01 00:33:06 +0800

    Added Update Operation

--- update.py
+++ update.py
@@ -1,10 +1,8 @@
 from server import db, c
 
 update_tbl = """
-    INSERT INTO `student_db`.`students`(first_name, last_name, age)
-    VALUES
-        ("Hannah", "Baniqued", 19),
-        ("Hannah", "Lei", 18)
+    UPDATE students
+    SET age = 19 WHERE age <= 18
 """
 
 c.execute(update_tbl)
