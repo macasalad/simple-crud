c066cd487ab0c56baddd5c1ac460d76dc13143a1
macasalad <hannahleibaniqued12@gmail.com>
2025-03-01 00:15:03 +0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root"
+)
+c = db.cursor()
+
+create_db = "CREATE DATABASE IF NOT EXISTS student_db"
+c.execute(create_db)
+db.close()
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "student_db"
+)
+c = db.cursor()
+db.close()
